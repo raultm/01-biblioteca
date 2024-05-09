a777bf4d8dabbc683a84570aff6930165884ab1b
Raul Tierno <raultm@gmail.com>
2024-05-09 12:45:38 +0200

    Router - rutas, error y layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 // MARK: App 
 
 import { Proyecto } from "./Proyecto"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Dashboard } from "./modules/Dashboard/pages/Dashboard";
+import { ErrorPage } from "./modules/ErrorPage/pages/ErrorPage";
+import { ErrorDashboardPage } from "./modules/ErrorPage/pages/ErrorDashboardPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Proyecto />,
+  //  errorElement: <ErrorPage/>
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
 
 function App() {
-  
+
   return (
     <>
-      <Proyecto />
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ src/modules/ErrorPage/pages/ErrorDashboardPage.jsx
@@ -0,0 +1,5 @@
+export const ErrorDashboardPage = () => {
+  return (
+    <div>ErrorDashboardPage</div>
+  )
+}
