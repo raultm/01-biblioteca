0663979db20c0eff5600f8914be90296ba1c1c7c
Raul Tierno <raultm@gmail.com>
2024-05-09 13:07:32 +0200

    Layout - set outlet

--- src/Proyecto.jsx
+++ src/Proyecto.jsx
@@ -1,19 +1,17 @@
 import { Content } from "./layout/Content"
 import { Header } from "./layout/Header"
 import { SideBar } from "./layout/SideBar"
-import { Dashboard } from "./modules/Dashboard/pages/Dashboard"
 
 export const Proyecto = () => {
   return (
     <main className="relative h-screen overflow-hidden bg-gray-100 dark:bg-gray-800">
       <div className="flex items-start justify-between">
-        <SideBar />
+        <SideBar/>
         <div className="flex flex-col w-full md:space-y-4">
-          <Header />
+          <Header/>
           <Content/>
         </div>
       </div>
     </main>
-
   )
 }
--- src/layout/Content.jsx
+++ src/layout/Content.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export const Content = () => {
   return (
-    <div>Content</div>
+    <div className="h-screen px-4 pb-24 overflow-auto md:px-6">
+        <Outlet/>
+    </div>
   )
 }
